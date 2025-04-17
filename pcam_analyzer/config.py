8f4e90b9b237910cf4fafac72027433f90cde8d7
Martina Hanusová <martina.hanusova@protonmail.com>
2025-04-17 22:30:11 +0200

    Remove pycache

--- pcam_analyzer/config.py
+++ pcam_analyzer/config.py
@@ -1,6 +1,6 @@
 DEFAULT_MODEL_JSON = "../models/gradcam_stain_standardization.json"
 DEFAULT_MODEL_WEIGHTS = "../models/gradcam_stain_standardization.hdf5"
-FALSE_POSITIVES_PATH = "../false_positives"
+FALSE_POSITIVES_PATH = "false_positives"
 
 IMAGE_SIZE = 96
 BATCH_SIZE = 32
